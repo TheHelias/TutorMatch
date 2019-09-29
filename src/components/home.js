c58988350a1d28504f9e9feb013a3cac539405db
Elias Akande <akandetoluwalase@gmail.com>
2019-09-29 13:20:21 +0100

    created and styled the homepage

---
+++ src/components/home.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import  { Link }   from 'react-router-dom'
+
+const Homepage = () => <div>
+    <div className='header'>
+        <div className='header_logo'>
+            <img src={process.env.PUBLIC_URL + '/tutormatch-logo.png'} alt="tutor"/>
+        </div>
+        <div className='header_links'>
+            <Link className='child'>Become a Tutor</Link>
+            <Link className='child sign_in'>Sign In</Link>
+        </div>
+    </div>
+
+    <div className='body'>
+        <div className="body_1">
+            <h1>The place where Tutors and Students meet</h1>
+            <p>Find out who can help take your education to the next level</p>
+            <Link to='/' className='link'>Get started now</Link>
+        </div>
+        <div className="body_2">
+            <h2>Our Tutors</h2>
+            <p>We have more than 100 tutors from various disciplines ready to match with you</p>
+        </div>
+        <div className='footer'>
+            <Link to='/' className='link'>See all Tutors</Link>
+        </div>
+    </div>
+</div> 
+
+export default Homepage;
